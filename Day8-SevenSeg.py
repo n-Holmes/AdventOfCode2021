d3dc0ae5801eab94c3e82ffb15d3a3d0b218b2b4
Nathan Holmes <naholmes93@gmail.com>
2021-12-08 16:55:49 +0000

    Day 8 part 2 by brute force

--- Day8-SevenSeg.py
+++ Day8-SevenSeg.py
@@ -1,44 +1,73 @@
+from itertools import permutations
+from timeit import default_timer as timer
+
 # The segments turned on for each digit
 DIGITS = [
-    {0, 1, 2, 4, 5, 6},
-    {2, 5},
-    {0, 2, 3, 4, 6},
-    {0, 2, 3, 5, 6},
-    {1, 2, 3, 5},
-    {0, 1, 3, 5, 6},
-    {0, 1, 3, 4, 5, 6},
-    {0, 2, 5},
-    {0, 1, 2, 3, 4, 5, 6},
-    {0, 1, 2, 3, 5, 6},
+    "abcefg",
+    "cf",
+    "acdeg",
+    "acdfg",
+    "bcdf",
+    "abdfg",
+    "abdefg",
+    "acf",
+    "abcdefg",
+    "abcdfg",
 ]
-
-DIGIT_LENS = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
 
 
 def get_outputs(line):
     _, o = line.split("|")
-    return o.strip()
+    return o.strip().split()
 
 
 def get_digits(line):
-    return line.strip().replace("| ", "").split()
+    """Get the coded digits from the input line.
+    Return a list sorted by length as the shortest are the most useful.
+    """
+    return sorted(line.strip().replace("| ", "").split(), key=len)
 
 
 def part_1(outputs):
-    count = sum(len(s) in [2, 3, 4, 7] for l in outputs for s in l.split())
+    count = sum(len(s) in [2, 3, 4, 7] for l in outputs for s in l)
     print(f"There are {count} identifiable output digits")
+
+
+def map_digit(coded_digit, encoding):
+    """Maps a coded digit to a value, using the given encoding"""
+    A = ord("a")
+
+    uncoded = "".join(sorted(encoding[ord(c) - A] for c in coded_digit.strip()))
+    try:
+        return DIGITS.index(uncoded)
+    except ValueError:
+        return None
 
 
 def decode_mapping(line):
     coded_digits = get_digits(line)
 
-    valid_mappings = {c: set(range(7)) for c in range(ord("a"), ord("g") + 1)}
+    for encoding in permutations("abcdefg", 7):
+        if all(map_digit(cd, encoding) is not None for cd in coded_digits):
+            # Found a valid encoding
+            coded_output = get_outputs(line)
+            output = int("".join(str(map_digit(cd, encoding)) for cd in coded_output))
 
-    for cd in coded_digits:
-        pass
+            # print(coded_output, output)
+            return output
+
+
+def part_2(lines):
+    print(sum(decode_mapping(line) for line in lines))
+
 
 if __name__ == "__main__":
+    start = timer()
+
     with open("input8.txt") as f:
         lines = f.readlines()
 
     part_1(map(get_outputs, lines))
+    part_2(lines)
+
+    print(f"Solution took {timer() - start}s") # 0.733s
